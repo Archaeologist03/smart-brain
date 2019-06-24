fd1a2b0c1c031a058650f1f6065781f167ed4f4f
Archaeologist03 <omniiknight@hotmail.com>
2019-06-24 17:03:28 +0200

    Adding/finished JWT auth. (sign in)

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -22,6 +22,10 @@ class Signin extends React.Component {
     this.setState({ signInPassword: event.target.value });
   };
 
+  saveAuthTokenInSession = token => {
+    window.sessionStorage.setItem('token', token);
+  };
+
   onSubmitSignIn = () => {
     fetch(`${apiUrl}/signin`, {
       method: 'post',
@@ -37,9 +41,22 @@ class Signin extends React.Component {
         return response.json();
       })
       .then(data => {
-        if (data.userId) {
-          this.props.loadUser(data);
-          this.props.onRouteChange('home');
+        if (data.userId && data.success === 'true') {
+          this.saveAuthTokenInSession(data.token);
+          fetch(`${apiUrl}/profile/${data.userId}`, {
+            method: 'get',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: data.token,
+            },
+          })
+            .then(resp => resp.json())
+            .then(user => {
+              if (user && user.email) {
+                this.props.loadUser(user);
+                this.props.onRouteChange('home');
+              }
+            });
         }
       })
       .catch(console.log);
